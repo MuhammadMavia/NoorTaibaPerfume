876a844b289ce5fdc7d34d58d3a7747757a39a53
Muhammad Mavia <Muhammadmavia172@gmail.com>
2025-04-26 17:05:21 +0500

    Updated

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,40 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CartProvider } from "@/context/CartContext";
 import { Toaster } from "@/components/ui/toaster";
 
+
+(function () {
+  if (window.fbq) return;
+
+  const fbq = function () {
+    fbq.callMethod
+      ? fbq.callMethod.apply(fbq, arguments)
+      : fbq.queue.push(arguments);
+  };
+
+  window.fbq = fbq;
+  fbq.push = fbq;
+  fbq.loaded = true;
+  fbq.version = '2.0';
+  fbq.queue = [];
+
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://connect.facebook.net/en_US/fbevents.js';
+  document.head.appendChild(script);
+
+  fbq('init', '679188314701986');
+  fbq('track', 'PageView');
+
+  // Fallback for users without JavaScript
+  const noscript = document.createElement('noscript');
+  noscript.innerHTML = `
+    <img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=679188314701986&ev=PageView&noscript=1" />
+  `;
+  document.body.appendChild(noscript);
+})();
+
+
 // Create a new QueryClient instance
 const queryClient = new QueryClient({
   defaultOptions: {
